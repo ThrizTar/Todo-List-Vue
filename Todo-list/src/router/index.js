440f4e6096060d7ea3ba78c59ef7a511926947f5
Thriz Tar <tarmasterz2545@gmail.com>
2024-03-22 15:50:19 +0700

    delete test vue page

--- Todo-list/src/router/index.js
+++ Todo-list/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EditView from '../views/EditView.vue'
-import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'todo-list',
       component: HomeView
-    },
-    {
-      path: '/test',
-      name: 'test-view',
-      component: TestView
     },
     {
       path: '/todo/:id/edit',
--- Todo-list/src/views/TestView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="text-sm md:text-lg">
-    Test text
-    </div>
-</template>
