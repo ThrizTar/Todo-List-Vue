b085f13f4480fc09d53855ceb217c9e0fd29e815
Thriz Tar <tarmasterz2545@gmail.com>
2024-03-23 10:34:43 +0700

    add style to edit page

--- Todo-list/src/views/EditView.vue
+++ Todo-list/src/views/EditView.vue
@@ -3,6 +3,8 @@ import { onMounted, ref } from 'vue';
 import { useTodoStore } from '../stores/todo'
 import { useRoute } from 'vue-router'
 
+import Loading from '../components/Loading.vue';
+
 const todoStore = useTodoStore()
 const route = useRoute()
 
@@ -31,28 +33,45 @@ const editTodo = async(selectedTodo) => {
 </script>
 
 <template>
-    <div>
-        Edit
-        <RouterLink :to="{ name: 'todo-list' }">
-            Back
-        </RouterLink>
+    <div class="w-1/2 mx-auto">
+        <div class="flex items-center">
+            <RouterLink :to="{ name: 'todo-list' }">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15">
+                <path d="M7.4 273.4C2.7 268.8 0 262.6 0 256s2.7-12.8 7.4-17.4l176-168c9.6-9.2 24.8-8.8 33.9 .8s8.8 24.8-.8 33.9L83.9 232 424 232c13.3 0 24 10.7 24 24s-10.7 24-24 24L83.9 280 216.6 406.6c9.6 9.2 9.9 24.3 .8 33.9s-24.3 9.9-33.9 .8l-176-168z"/></svg>
+            </RouterLink>
+            <div class="ml-2">Edit Page</div> 
+        </div>
         <div v-if="isLoaded">
-            id {{ todoId }}
-            <div>
-                Name: <input type="text" v-model="todoStore.selectedTodo.name">
+            id <div class="badge badge-neutral">{{ todoId }}</div>
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Name</span>
+                </label>
+                <input type="text" 
+                placeholder="Type name todo" 
+                class="input input-bordered w-full" 
+                v-model="todoStore.selectedTodo.name"/>
             </div>
-            <div>
-                Status: 
-                <select v-model="todoStore.selectedTodo.status">
+            <div class="form-control w-full">
+                <label class="label">
+                    <span class="label-text">Status</span>
+                </label>
+                <select class="select select-bordered" v-model="todoStore.selectedTodo.status">
                     <option v-for="status in todoStore.statuses" :value="status">{{ status }}</option>
                 </select>
             </div>
-            <div>
-                <button @click="editTodo(todoStore.selectedTodo)">Edit</button>
+            <div class="flex">
+                <button class="btn btn-primary w-full mt-4" @click="editTodo(todoStore.selectedTodo)">Edit</button>
             </div>
         </div> 
         <div v-else>
-            <h2>Loading...</h2>
+            <Loading></Loading>
         </div>
     </div>
 </template>
+
+<style scoped>
+    svg{
+        fill: white;
+    }
+</style>
